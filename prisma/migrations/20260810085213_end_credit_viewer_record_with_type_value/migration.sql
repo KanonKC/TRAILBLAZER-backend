b9fe4d2d0eeadc9f5f9d5d31d54c5dd243576b0b
KanonKC <kanonkce@gmail.com>
2026-08-13 23:36:11 +0700

    feat: update end credit schema

---
+++ prisma/migrations/20260810085213_end_credit_viewer_record_with_type_value/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `action` on the `EndCreditViewerRecord` table. All the data in the column will be lost.
+  - Added the required column `type` to the `EndCreditViewerRecord` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `value` to the `EndCreditViewerRecord` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "EndCreditViewerRecord" DROP COLUMN "action",
+ADD COLUMN     "type" TEXT NOT NULL,
+ADD COLUMN     "value" TEXT NOT NULL;
